76a4e9f9ee9622f1e057d956dcc2214ed7bf4045
jaehui327 <jaeheekim970327@gmail.com>
2021-01-17 23:35:37 +0900

    ✨ BehaviorSubject - tableView, label control

--- RxSwift+MVVM 4시간에 끝내기 시즌2/step3+Empty/RxSwift+MVVM/Pages/MenuList/MenuListViewModel.swift
+++ RxSwift+MVVM 4시간에 끝내기 시즌2/step3+Empty/RxSwift+MVVM/Pages/MenuList/MenuListViewModel.swift
@@ -11,18 +11,23 @@ import RxSwift
 
 class MenuListViewModel {
     
-    var menus: [Menu] = [
-        Menu(name: "튀김1", price: 100, count: 0),
-        Menu(name: "튀김1", price: 100, count: 0),
-        Menu(name: "튀김1", price: 100, count: 0),
-        Menu(name: "튀김1", price: 100, count: 0)
-    ]
+    var menuObservable = BehaviorSubject<[Menu]>(value: [])
     
-    lazy var menuObsevable = Observable.just(menus)
-    
-    var itemsCount: Int = 5
-    var totalPrice: PublishSubject<Int> = PublishSubject()
-    
-    // Subject
-    
+    lazy var itemsCount = menuObservable.map {
+        $0.map { $0.count }.reduce(0, +)
+    }
+    lazy var totalPrice = menuObservable.map {
+        $0.map { $0.price * $0.count }.reduce(0, +)
+    }
+
+    init() {
+        let menus: [Menu] = [
+            Menu(name: "튀김1", price: 100, count: 0),
+            Menu(name: "튀김1", price: 100, count: 0),
+            Menu(name: "튀김1", price: 100, count: 0),
+            Menu(name: "튀김1", price: 100, count: 0)
+        ]
+        
+        menuObservable.onNext(menus)
+    }
 }
--- RxSwift+MVVM 4시간에 끝내기 시즌2/step3+Empty/RxSwift+MVVM/Pages/MenuList/MenuViewController.swift
+++ RxSwift+MVVM 4시간에 끝내기 시즌2/step3+Empty/RxSwift+MVVM/Pages/MenuList/MenuViewController.swift
@@ -13,18 +13,35 @@ import RxCocoa
 class MenuViewController: UIViewController {
     // MARK: - Life Cycle
     
+    let cellId = "MenuItemTableViewCell"
+    
     let viewModel = MenuListViewModel()
     var disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        viewModel.menuObservable
+            .observeOn(MainScheduler.instance)
+            .bind(to: tableView.rx.items(cellIdentifier: cellId, cellType: MenuItemTableViewCell.self)) { index, item, cell in
+                
+                cell.title.text = item.name
+                cell.price.text = "\(item.price)"
+                cell.count.text = "\(item.count)"
+                
+            }
+            .disposed(by: disposeBag)
+        
+        viewModel.itemsCount
+            .map { "\($0)" }
+            .observeOn(MainScheduler.instance)
+            .bind(to: itemCountLabel.rx.text)
+            .disposed(by: disposeBag)
+        
         viewModel.totalPrice
-            .scan(0, accumulator: +)
             .map { $0.currencyKR() }
-            .subscribe(onNext: {
-                self.totalPrice.text = $0
-            })
+            .observeOn(MainScheduler.instance)
+            .bind(to: totalPrice.rx.text)
             .disposed(by: disposeBag)
     }
 
@@ -57,23 +74,10 @@ class MenuViewController: UIViewController {
         // showAlert("Order Fail", "No Orders")
 //        performSegue(withIdentifier: "OrderViewController", sender: nil)
         
-        viewModel.totalPrice.onNext(100)
-    }
-}
-
-extension MenuViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel.menus.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MenuItemTableViewCell") as! MenuItemTableViewCell
-
-        let menu = viewModel.menus[indexPath.row]
-        cell.title.text = menu.name
-        cell.price.text = "\(menu.price)"
-        cell.count.text = "\(menu.count)"
-
-        return cell
+        viewModel.menuObservable.onNext([
+            Menu(name: "changed", price: Int.random(in: 100...1_000), count: Int.random(in: 0...3)),
+            Menu(name: "changed", price: Int.random(in: 100...1_000), count: Int.random(in: 0...3)),
+            Menu(name: "changed", price: Int.random(in: 100...1_000), count: Int.random(in: 0...3))
+        ])
     }
 }
