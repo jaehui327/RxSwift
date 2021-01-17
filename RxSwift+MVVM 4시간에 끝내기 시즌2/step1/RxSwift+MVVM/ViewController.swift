72077693d4db3b0d1b24e87d91641d75ecdb84f7
jaehui327 <jaeheekim970327@gmail.com>
2021-01-17 16:00:45 +0900

    🔧 networking 함수로 분리, 클로저

--- RxSwift+MVVM 4시간에 끝내기 시즌2/step1/RxSwift+MVVM/ViewController.swift
+++ RxSwift+MVVM 4시간에 끝내기 시즌2/step1/RxSwift+MVVM/ViewController.swift
@@ -32,23 +32,28 @@ class ViewController: UIViewController {
         })
     }
     
+    func downloadJson(_ url: String, _ completion: ((String?) -> Void)?) {
+        DispatchQueue.global().async {
+            let url = URL(string: url)!
+            let data = try! Data(contentsOf: url)
+            let json = String(data: data, encoding: .utf8)
+            DispatchQueue.main.async {
+                completion?(json)
+            }
+        }
+    }
+    
     // MARK: SYNC
-
+    
     @IBOutlet var activityIndicator: UIActivityIndicatorView!
-
+    
     @IBAction func onLoad() {
         editView.text = ""
         setVisibleWithAnimation(activityIndicator, true)
         
-        DispatchQueue.global().async {
-            let url = URL(string: MEMBER_LIST_URL)!
-            let data = try! Data(contentsOf: url)
-            let json = String(data: data, encoding: .utf8)
-            
-            DispatchQueue.main.async {
-                self.editView.text = json
-                self.setVisibleWithAnimation(self.activityIndicator, false)
-            }
+        downloadJson(MEMBER_LIST_URL) { json in
+            self.editView.text = json
+            self.setVisibleWithAnimation(self.activityIndicator, false)
         }
     }
 }
